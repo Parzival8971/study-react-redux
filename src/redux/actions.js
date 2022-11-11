8dad1d21a38887e113afb54602d787bc39d719ce
Parzival8971 <esh8971@gmail.com>
2022-11-11 19:06:48 +0900

    redux-thunk를 만든다

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 
@@ -55,5 +57,17 @@ export function getUsersFail(error) {
   return {
     type: GET_USERS_FAIL,
     error,
+  };
+}
+
+export function getUsersThunk() {
+  return async (dispatch) => {
+    try {
+      dispatch(getUsersStart());
+      const res = await axios.get('https://api.github.com/users');
+      dispatch(getUsersSuccess(res.data));
+    } catch (error) {
+      dispatch(getUsersFail(error));
+    }
   };
 }
